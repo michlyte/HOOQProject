0f08f4bd24b7b9de9f010bdc1ef2289fc2d81109
michaelhalim <mikefla10@gmail.com>
2018-05-12 21:55:32 +0700

    Movie Detail

---
+++ HOOQProject/model/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  HOOQProject
+//
+//  Created by Michael Halim on 12/05/18.
+//  Copyright © 2018 com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    // MARK: Properties
+    @IBOutlet weak var movieImage: UIImageView!
+}
--- HOOQProject/viewController/MovieViewController.swift
+++ HOOQProject/viewController/MovieViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import Alamofire
+import JSONJoy
 import Kingfisher
 
-class MovieViewController: UIViewController {
-
+class MovieViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     // MARK: Properties
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var movieImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var overviewTitleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var relatedMovieCollectionView: UICollectionView!
     
     var movie: MovieJSON?
+    
+    private var datasource: SimilarMovieJSON?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,12 +49,59 @@ class MovieViewController: UIViewController {
             }
             scoreLabel.text = "\(movie.vote_average)"
             overviewLabel.text = movie.overview
+            
+            Alamofire.request(RestAPI.getSimilarMovieURL(movie_id: "\(movie.id)"), method: .get, parameters: RestAPI.getParams(page: "1"), encoding: URLEncoding.default, headers: nil)
+                .responseJSON(completionHandler: { (response) in
+                    switch response.result {
+                    case .success(let result):
+                        //do the checking with expected result
+                        //AssertEqual or whatever you need to do with the data
+                        //finally fullfill the expectation
+                        do {
+                            self.datasource = try SimilarMovieJSON(JSONLoader(result))
+                            
+                            DispatchQueue.main.async {
+                                self.relatedMovieCollectionView.reloadData()
+                            }
+                        } catch {
+                            print("unable to parse the JSON")
+                        }
+                    case .failure(let error):
+                        print(error)
+                    }
+                })
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UICollectionView
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let datasource = datasource {
+            return datasource.results.count
+        } else {
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let movieCell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath) as? MovieCollectionViewCell {
+            
+            if let datasource = datasource, let poster = datasource.results[indexPath.row].poster_path, let imageUrl: URL = URL(string: RestAPI.getPosterPathURL(poster_path: poster)) {
+                movieCell.movieImage.kf.setImage(with: imageUrl)
+            }
+            
+            return movieCell
+        } else {
+            return UICollectionViewCell()
+        }
     }
     
 
